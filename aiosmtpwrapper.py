8c9515d26119890621cbe95cc8d507821c898288
Aleksey <a.alexey.lazarev@gmail.com>
2019-07-25 18:12:29 +0300

    Some changes in aiosmtp

--- aiosmtpwrapper.py
+++ aiosmtpwrapper.py
@@ -1,56 +1,65 @@
 import asyncio
+import time
 import aiosmtplib
-import logging
-logging.basicConfig(filename="aios.log", level=logging.DEBUG, filemode='w')
+from classes import cfg, Email
 
-class SMTPError(Exception):
-    pass
+def create_tasks(trs: list,fun):
+    tasks=[]
+    tasks=[]
+    for tr in trs:
+        tasks.append(asyncio.create_task(fun(tr)))
+    return tasks
 
+def tic():
+    return '%1.1f' % (time.time() - start)
+
+async def test(*args,**kwargs):
+    print('test started work: {}'.format(tic()))
+    res = await asyncio.sleep(1, result='test')
+    print('test end work: {}'.format(tic()))
+    return res
 
 class SMTP:
+    def __init__(self, hostname, port, username, password, use_tls = False):
+        self.hostname=hostname
+        self.port = port
+        self.username = username
+        self.password = password
+        self.use_tls = use_tls
+        self.smtp = aiosmtplib.SMTP(
+            hostname=self.hostname, port=self.port, use_tls=self.use_tls)
 
-    def __init__(self, hostname, port, username, password, use_tls=True):
-        self.loop = asyncio.get_event_loop()
-        try:
-            self.smtp = aiosmtplib.SMTP(hostname=hostname, port=port, loop=self.loop , use_tls=use_tls)
-            self.loop.run_until_complete(self.smtp.connect())
-            self.loop.run_until_complete(self.smtp._ehlo_or_helo_if_needed())
-            self.loop.run_until_complete(self.smtp.auth_login(username=username, password=password))
-        except Exception as e:
-            print(e.__str__())
+    async def connect(self):
+        #Connect to smtp
+        await self.smtp.connect()
+        await self.smtp.auth_login(username=self.username, password=self.password)
 
-    def send(self, messages: list):
-        self.tasks=[]
-        for message in messages:
-            self.tasks.append(self.loop.create_task(self.smtp.send_message(message)))
-            try:
-                result = self.loop.run_until_complete(self.tasks)
-                print('message send')
-            except (aiosmtplib.SMTPRecipientsRefused) as e:
-                print(e.__str__())
-            except (TypeError):
-                print(e.__str__())
-            except Exception as e:
-                print(e.__str__())
+        # send emails via creation connect
+    async def sending(self,messages):
+        result = []
+        for task in create_tasks(messages, self.smtp.send_message):
+            res = await task
+            result.append(res)
         return result
-            
-    def __del__(self):
-        try:
-            self.loop.run_until_complete(self.smtp.quit())
-        except Exception as e:
-            print(e.__str__())
 
+async def main(cfg,messages):
+    smtp=SMTP(**cfg.smtp)
+    connect_task = asyncio.create_task(smtp.connect())
+    await connect_task
+    send_task = asyncio.create_task(smtp.sending(messages))
+    response = await send_task
+    print(response)
 
-if __name__ == "__main__":
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-    import time
-    from classes import cfg, Email
-    messages=[]
-    for i in range(0,5):
-        body = '<p><br>Privet Тут и русский есть?Б<br>ya uje %sый<br>%s</p>' % (i,time.ctime())
-        message = Email(to=cfg.smtp['username'],From=cfg.smtp['username'],body=body,subject="Test from aiosmtp %s" % i)
+messages = []
+for i in range(0, 5):
+        body = '<p><br>Privet Тут и русский есть?Б<br>ya uje %sый<br>%s</p>' % (
+            i, time.ctime())
+        message = Email(**cfg.send, body=body,
+                        subject="Test from aiosmtp %s" % i)
         messages.append(message)
-    print(messages)
-    test = SMTP(cfg.smtp['host'],cfg.smtp['port'],cfg.smtp['username'],cfg.smtp['password'],use_tls=True).send(messages)
-    print(test)
+start = time.time()
+asyncio.run(main(cfg,messages))
+#print(tic())
+#print(200/float(tic()))
+
+#Email1 = Email(**cfg.send, body=body, subject="Test from aiosmtp %s" % 1)
